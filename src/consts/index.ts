f0fb52a22eba5a6db0f69b9439f2cc189726f27a
Anass Ez-zouaine <ans-ezzouaine@hotmail.com>
2026-09-14 13:53:39 +0800

    fix: update consts barrel + blog meta module

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,7 +1,10 @@
 /**
  * Site constants barrel. Split into modules for maintainability; import from `@/consts` as before.
  */
-export * from "./01-core";
+export * from "./01a-site";
+export * from "./01a2-blog-series";
+export * from "./01b-blog-meta";
 export * from "./02-blog-tags";
 export * from "./03-services-now";
-export * from "./04-tools-library";
+export * from "./04a-tools";
+export * from "./04b-library";
