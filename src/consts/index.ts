e66f50bed8acec4b2e72d2094d820c6941873fcb
Anass Ez-zouaine <ans-ezzouaine@hotmail.com>
2026-09-14 13:51:31 +0800

    fix: restore consts modules (index + services/now) with P0/P1 content

---
+++ src/consts/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Site constants barrel. Split into modules for maintainability; import from `@/consts` as before.
+ */
+export * from "./01-core";
+export * from "./02-blog-tags";
+export * from "./03-services-now";
+export * from "./04-tools-library";
